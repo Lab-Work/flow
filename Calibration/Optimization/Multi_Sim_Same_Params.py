6b368a0cc940c34c28af2ccc9a3413f21388c20c
George Gunter <gunter.gl@gmail.com>
2020-07-16 12:05:04 -0500

    updated multi_sim to get multiple fidelities when sampling

--- Calibration/Optimization/Multi_Sim_Same_Params.py
+++ Calibration/Optimization/Multi_Sim_Same_Params.py
@@ -4,28 +4,26 @@ import highway_congested as hc
 import time, random, csv, os, sys
 
 
-real_params = [0.3,1.67]
+real_params = [0.53,1.67]
 
 num_sims = 50
 
 counts = []
 speeds = []
 
-<<<<<<< HEAD
+
 num_samples_from_end = 45
 sim_length =  4500
-=======
-num_samples_from_end = 15
->>>>>>> 559ca576eb26d3cf2e1eaecae7936754ad6c75ce
+
+fidelity_levels = []
+
+count_dict = {}
 
 for i in range(num_sims):
 	#Simulatees many times using the same parameters to examine stochasticity in the model:
 	print('Simulation Number: '+str(i))
-<<<<<<< HEAD
 	real_results = hc.HighwayCongested(wave_params=real_params,sim_length=sim_length)
-=======
 	real_results = hc.HighwayCongested(wave_params=real_params)
->>>>>>> 559ca576eb26d3cf2e1eaecae7936754ad6c75ce
 	real_counts = np.array(real_results.getCountsData())
 	real_speeds = np.array(real_results.getVelocityData())
 
